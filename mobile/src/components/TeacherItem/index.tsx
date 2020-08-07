c28f07ef5fae6c16b93b41091bd1b5ee7f261d5f
Tulio Camargo <tuliocaz.windcoast@gmail.com>
2020-08-07 15:01:52 -0300

    feat: TeacherItem component created

---
+++ mobile/src/components/TeacherItem/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/TulioCaz.png' }}
+        />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Túlio Camargo</Text>
+          <Text style={styles.subject}>Química</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Enthusiast of absolutely everything related to technology & Passionate
+        about solving problems.
+        {'\n'}
+        {'\n'}
+        Fullstack Developer, self-taught. I spent the last few days after a
+        professional training, now I am looking to fit into the job market in
+        some technical position of software development.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ 80,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default TeacherItem;
